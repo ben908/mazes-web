2e4ae1e04ba0cd466c5fc90fcf427a2332889332
Ben C <clarage2@illinois.edu>
2021-12-14 03:43:37 -0600

    3D Maze Render Implementation

--- main.js
+++ main.js
@@ -41,13 +41,14 @@ const axesHelper = new THREE.AxesHelper(5000);
 pointLight.position.set(0, 0, 0);
 axesHelper.setColors(new THREE.Color(0xff0000), new THREE.Color(0x00ff00), new THREE.Color(0x0000ff))
 
-const WALL_SIZE = 1
+let WALL_SIZE = 0.75
+const TOTAL_WALL_SIZE = 1
 
 const wall_geometry = new THREE.BoxBufferGeometry(WALL_SIZE, WALL_SIZE, WALL_SIZE);
 const wall_material = new THREE.MeshBasicMaterial({
   color: 0xcccccc,
   opacity: 0.6,
-  transparent: true,
+  transparent: false,
   blending: THREE.CustomBlending,
   blendEquation: THREE.AddEquation,
   blendSrc: THREE.SrcAlphaFactor,
@@ -62,7 +63,7 @@ const sols_geometry = new THREE.BoxBufferGeometry(WALL_SIZE, WALL_SIZE, WALL_SIZ
 const sols_material = new THREE.MeshBasicMaterial({
   color: 0xD82929,
   opacity: 1,
-  transparent: true,
+  transparent: false,
   blending: THREE.CustomBlending,
   blendEquation: THREE.AddEquation,
   blendSrc: THREE.SrcAlphaFactor,
@@ -86,11 +87,11 @@ function addMazeWalls(dims, maze_vec) {
   var dummy = new THREE.Object3D();
 
   for (var i = 0; i < 2 * dims[0] + 1; i++) {
-    addCube(mesh, dummy, [i * WALL_SIZE, 0, 0], i)
+    addCube(mesh, dummy, [i * TOTAL_WALL_SIZE, 0, 0], i)
   }
 
   for (var j = 0; j < 2 * dims[1] + 1; j++) {
-    addCube(mesh, dummy, [0, 0, j * WALL_SIZE], j * (dims[0] * 2 + 1))
+    addCube(mesh, dummy, [0, 0, j * TOTAL_WALL_SIZE], j * (dims[0] * 2 + 1))
   }
 
   for (var j = 0; j < dims[1]; j++) {
@@ -104,17 +105,55 @@ function addMazeWalls(dims, maze_vec) {
       var y = 2 * j + 1
       //0, 1 = right wall s.getWallSide(1)
       if (s.getWallSide(0)) {
-        addCube(mesh, dummy, [(x + 1) * WALL_SIZE, 0, y * WALL_SIZE], y * x_with + (x + 1))
+        addCube(mesh, dummy, [(x + 1) * TOTAL_WALL_SIZE, 0, y * TOTAL_WALL_SIZE], y * x_with + (x + 1))
       }
       //1, 0 = down wall s.getWallSide(0)
       if (s.getWallSide(1)) {
-        addCube(mesh, dummy, [x * WALL_SIZE, 0, (y + 1) * WALL_SIZE], (y + 1) * x_with + x)
+        addCube(mesh, dummy, [x * TOTAL_WALL_SIZE, 0, (y + 1) * TOTAL_WALL_SIZE], (y + 1) * x_with + x)
       }
 
       //1, 1 always has cube
-      addCube(mesh, dummy, [(x + 1) * WALL_SIZE, 0, (y + 1) * WALL_SIZE], (y + 1) * x_with + (x + 1))
+      addCube(mesh, dummy, [(x + 1) * TOTAL_WALL_SIZE, 0, (y + 1) * TOTAL_WALL_SIZE], (y + 1) * x_with + (x + 1))
     }
   }
+}
+
+
+function addMazeWalls3D(dims, maze) {
+  const x_with = (dims[0] * 2 + 1)
+  const y_width = dims[1] * 2 + 1
+
+  var mesh = new THREE.InstancedMesh(wall_geometry, wall_material, (dims[0] * dims[1] * dims[2] * 8));
+
+  allMeshes.push(mesh)
+  scene.add(mesh);
+
+  var dummy = new THREE.Object3D();
+  const maze_vec = maze.getMaze()
+  const indexToPoint = maze.getIndexToPointVector()
+
+  for (let i = 0; i < maze_vec.size(); ++i) {
+    let curSquare = maze_vec.get(i);
+    let curPoint = indexToPoint.get(i);
+    //0,0 always empty
+    if (curSquare.getWallSide(0)) {
+      addCube(mesh, dummy, [curPoint.get(0) * 2 + 1, -1 * curPoint.get(2) * 2, curPoint.get(1) * 2], 8*i)
+    }
+    if (curSquare.getWallSide(1)) {
+      addCube(mesh, dummy, [curPoint.get(0) * 2, -1 * curPoint.get(2) * 2, curPoint.get(1) * 2 + 1], 8*i + 1)
+    }
+    if (curSquare.getWallSide(2)) {
+      addCube(mesh, dummy, [curPoint.get(0) * 2, -1 * curPoint.get(2) * 2 - 1, curPoint.get(1) * 2], 8*i + 2)
+    }
+
+    addCube(mesh, dummy, [curPoint.get(0) * 2 + 1, -1 * curPoint.get(2) * 2, curPoint.get(1) * 2 + 1], 8*i + 3)
+    addCube(mesh, dummy, [curPoint.get(0) * 2 + 1, -1 * curPoint.get(2) * 2 - 1, curPoint.get(1) * 2], 8*i + 4)
+    addCube(mesh, dummy, [curPoint.get(0) * 2 + 1, -1 * curPoint.get(2) * 2 - 1, curPoint.get(1) * 2 + 1], 8*i + 5)
+    addCube(mesh, dummy, [curPoint.get(0) * 2, -1 * curPoint.get(2) * 2 - 1, curPoint.get(1) * 2 + 1], 8*i + 6)
+  }
+
+  maze_vec.delete()
+  indexToPoint.delete()
 }
 
 function addMazeSolution(dims, solution) {
@@ -128,44 +167,108 @@ function addMazeSolution(dims, solution) {
   var curr_y = 1;
   var vec = new THREE.Object3D();
 
-  addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], solution.size() * 2)
+  addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], solution.size() * 2)
 
   for (let i = 0; i < solution.size(); ++i) {
     let dir = solution.get(i);
     let index = i * 2
     if (dir == 0) {
       curr_x++;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index)
 
       curr_x++;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index + 1)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index + 1)
 
     } else if (dir == 1) {
       curr_x--
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index)
 
       curr_x--;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index + 1)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index + 1)
 
     } else if (dir == 2) {
       curr_y++;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index)
 
       curr_y++;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index + 1)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index + 1)
 
     } else if (dir == 3) {
       curr_y--;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index)
 
       curr_y--;
-      addCube(mesh, vec, [curr_x * WALL_SIZE, 0, curr_y * WALL_SIZE], index + 1)
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], index + 1)
 
     } else {
       throw Error("Should never execute")
     }
   }
 }
+
+function addMazeSolution3D(dims, solution) {
+  var mesh = new THREE.InstancedMesh(sols_geometry, sols_material, (solution.size() * 2) + 1);
+
+  allMeshes.push(mesh)
+  scene.add(mesh);
+  var curr_x = 0;
+  var curr_y = 0;
+  var curr_z = 0;
+  var vec = new THREE.Object3D();
+
+  addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, 0, curr_y * TOTAL_WALL_SIZE], solution.size() * 2)
+
+  for (let i = 0; i < solution.size(); ++i) {
+    let dir = solution.get(i);
+    let index = i * 2
+    if (dir == 0) {
+      curr_x++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_x++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else if (dir == 1) {
+      curr_x--
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_x--;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else if (dir == 2) {
+      curr_y++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_y++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else if (dir == 3) {
+      curr_y--;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_y--;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else if (dir == 4) {
+      curr_z--;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_z--;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else if (dir == 5) {
+      curr_z++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index)
+
+      curr_z++;
+      addCube(mesh, vec, [curr_x * TOTAL_WALL_SIZE, curr_z * TOTAL_WALL_SIZE, curr_y * TOTAL_WALL_SIZE], index + 1)
+
+    } else {
+      throw Error("Should never execute")
+    }
+  }
+}
+
 
 function addCube(mesh, vec, position, index) {
   vec.position.x = position[0]
@@ -190,14 +293,15 @@ const params = {
   mazeWallOpacity: 0.1,
   showMazeSolution: true,
   mazeSolutionOpacity: 1,
+  cubeSideLength: 0.75,
   generateNewMaze: generateNewMaze,
   updateCameraPositionOnNewGenerate: true
 }
 
 let dimParams = {
-  dim1: 20,
-  dim2: 20,
-  dim3: 0,
+  dim1: 2,
+  dim2: 2,
+  dim3: 2,
   dim4: 0,
   dim5: 0,
   dim6: 0,
@@ -253,14 +357,23 @@ function init() {
   gui.add(params, 'mazeSolutionOpacity', 0.0, 1.0, 0.01).onChange(value => {
     sols_material.opacity = value
   });
+  gui.add(params, 'cubeSideLength', 0.01, 1.0, 0.01).onChange(value => {
+    let desired_size = value
+    let cur_size = WALL_SIZE;
+    let scale = desired_size/cur_size
+    WALL_SIZE = desired_size
+    sols_geometry.scale(scale, scale, scale)
+    wall_geometry.scale(scale, scale, scale)
+  });
+  
 
   gui.add(params, 'generateNewMaze');
   gui.add(params, 'updateCameraPositionOnNewGenerate')
 
   h = gui.addFolder("Dimension Sizes");
 
-  h.add(dimParams, "dim1", 2, 200, 1).name("Dim1")
-  h.add(dimParams, "dim2", 2, 200, 1).name("Dim2")
+  h.add(dimParams, "dim1", 0, 200, 1).name("Dim1")
+  h.add(dimParams, "dim2", 0, 200, 1).name("Dim2")
   h.add(dimParams, "dim3", 0, 200, 1).name("Dim3")
   h.add(dimParams, "dim4", 0, 200, 1).name("Dim4")
   h.add(dimParams, "dim5", 0, 200, 1).name("Dim5")
@@ -273,7 +386,6 @@ function init() {
 function generateNewMaze() {
   clean()
   createModule().then(({ SquareMaze, Int1dVec }) => {
-    // Hardcoded input values
     var dims = []
     let total = 1
     Object.entries(dimParams).forEach(([k, v]) => {
@@ -285,42 +397,56 @@ function generateNewMaze() {
     if (total > 250000) {
       if (!confirm("WARNING \nYou are trying to solve a maze with " + total.toLocaleString()
         + " positions which has at least " + (total * 5).toLocaleString()
-        + " cubes that will be rendered, not including the solution cubes."
-        + " The maximum recommended number of cubes to render is " + (250000).toLocaleString()
-        + "\nDo you wish to continue?"
-        + "\n(It will take a while to load)")) {
+        + " walls cubes that would be rendered."
+        + "\nMazes with more than " + (1000000).toLocaleString()
+        + " expected walls will not generate walls no matter what"
+        + "\nThis action will take an expected time of: "
+        + (2*total/1000000).toLocaleString() + " seconds."
+        + "\n(time estimate based on testing on 10th Gen Intel i7)"
+        + "\nDo you wish to continue?")) {
         console.log("Maze gen terminated");
         return
       }
     }
 
     console.time("MazeGen")
-    const test = new SquareMaze(dims.length);
+    const maze = new SquareMaze(dims.length);
     const vec = new Int1dVec();
     for (let i = 0; i < dims.length; ++i) {
       vec.push_back(dims[i]);
     }
-    test.makeMaze(vec);
+    maze.makeMaze(vec);
     console.timeEnd("MazeGen")
     console.time("MazeSolve")
 
 
-    const sols = test.solveMaze();
-    const squares = test.getMaze();
+    const sols = maze.solveMaze();
+    const squares = maze.getMaze();
     console.timeEnd("MazeSolve")
-    console.time("MazeRender")
-    addMazeWalls(dims, squares);
-    addMazeSolution(dims, sols);
+    console.time("MazeRender");
+
+    console.log("Vec Size: " + vec.size())
+
+    if (vec.size() == 2) {
+      addMazeWalls(dims, squares);
+      addMazeSolution(dims, sols);
+    } else {
+      if (total * 5 <= 1000000) {
+        addMazeWalls3D(dims, maze);
+      }
+      addMazeSolution3D(dims, sols);
+    }
+
     console.timeEnd("MazeRender")
     console.log("\n")
 
     vec.delete();
-    test.delete();
+    maze.delete();
     sols.delete();
     squares.delete();
   });
   if (params.updateCameraPositionOnNewGenerate) {
-    controls.target.set(dimParams.dim1, 0, dimParams.dim2);
+    controls.target.set(dimParams.dim1, -1 * dimParams.dim3, dimParams.dim2);
     camera.position.set(dimParams.dim1, (dimParams.dim2 + dimParams.dim1), dimParams.dim2);
     controls.update();
   }
